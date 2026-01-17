d5966d667945dca337aa98a1213dd9d61d7fd196
skillchair <strucnjak@elfak.rs>
2026-01-17 17:53:46 +0100

    pomocna f-ja za proveru suseda

--- game/board.py
+++ game/board.py
@@ -9,12 +9,42 @@ class Board:
         # minimalan broj poteza pre zavrsetka igre je 4(size-1)-2
         # sto je oba igraca po 2(n-1)-1 poteza
 
-        self.matrix = [
-            [0 for _ in range(2 * size - 1)]
-            for _ in range(2 * size - 1)
+        dim = 2 * size - 1
+        matrix = [
+            [0 for _ in range(dim)]
+            for _ in range(dim)
         ]
 
     def X(x):
         return x + 1
+    
     def Y(y):
         return chr(ord('A') + y)
+    
+    def checkRoots(self):
+        roots = []
+        # TODO: implementiraj f-ju koja nalazi sve korene koordinate
+
+    def checkNeighbors(self, x, y):
+        neighbors = []
+        if (x > 0):
+            if (y > 0):
+                if (self.matrix[x - 1][y - 1] == self.matrix[x][y]):
+                    neighbors.add(x - 1 + "," + y - 1)
+                if (self.matrix[x][y - 1] == self.matrix[x][y]):
+                    neighbors.add(x + "," + y - 1)
+
+            if (self.matrix[x - 1][y] == self.matrix[x][y]):
+                neighbors.add(x - 1 + "," + y)
+
+        if (x < self.dim - 1):
+            if (y < self.dim - 1):
+                if (self.matrix[x + 1][y + 1] == self.matrix[x][y]):
+                    neighbors.add(x + 1 + "," + y + 1)
+                if (self.matrix[x][y + 1] == self.matrix[x][y]):
+                    neighbors.add(x + "," + y + 1)
+
+            if (self.matrix[x + 1][y] == self.matrix[x][y]):
+                neighbors.add(x + 1 + "," + y)
+            
+        return neighbors
