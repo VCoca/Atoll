1480e0c5152c46b852518aa6df91d7e9c4172f85
Jovan Jovanović <jovanovicjovan@elfak.rs>
2026-01-18 11:45:08 +0100

    dodat checkRoots

--- game/board.py
+++ game/board.py
@@ -9,10 +9,10 @@ class Board:
         # minimalan broj poteza pre zavrsetka igre je 4(size-1)-2
         # sto je oba igraca po 2(n-1)-1 poteza
 
-        dim = 2 * size - 1
-        matrix = [
-            [0 for _ in range(dim)]
-            for _ in range(dim)
+        self.dim = 2 * size - 1
+        self.matrix = [
+            [0 for _ in range(self.dim)]
+            for _ in range(self.dim)
         ]
 
     def X(x):
@@ -22,29 +22,38 @@ class Board:
         return chr(ord('A') + y)
     
     def checkRoots(self):
-        roots = []
-        # TODO: implementiraj f-ju koja nalazi sve korene koordinate
+        n = self.size
+        roots_coords = [
+            (0, 0),
+            (0, n-1),
+            (2*(n-1), n-1),
+            (2*(n-1), 2*(n-1)),
+            (n-1, 2*(n-1)),
+            (0, n-1)
+        ]
+        roots = [f"{x},{y}" for x, y in roots_coords]
+        return roots
 
     def checkNeighbors(self, x, y):
         neighbors = []
         if (x > 0):
             if (y > 0):
                 if (self.matrix[x - 1][y - 1] == self.matrix[x][y]):
-                    neighbors.add(x - 1 + "," + y - 1)
+                    neighbors.append(str(x - 1) + "," + str(y - 1))
                 if (self.matrix[x][y - 1] == self.matrix[x][y]):
-                    neighbors.add(x + "," + y - 1)
+                    neighbors.append(str(x) + "," + str(y - 1))
 
             if (self.matrix[x - 1][y] == self.matrix[x][y]):
-                neighbors.add(x - 1 + "," + y)
+                neighbors.append(str(x - 1) + "," + str(y))
 
         if (x < self.dim - 1):
             if (y < self.dim - 1):
                 if (self.matrix[x + 1][y + 1] == self.matrix[x][y]):
-                    neighbors.add(x + 1 + "," + y + 1)
+                    neighbors.append(str(x + 1) + "," + str(y + 1))
                 if (self.matrix[x][y + 1] == self.matrix[x][y]):
-                    neighbors.add(x + "," + y + 1)
+                    neighbors.append(str(x) + "," + str(y + 1))
 
             if (self.matrix[x + 1][y] == self.matrix[x][y]):
-                neighbors.add(x + 1 + "," + y)
+                neighbors.append(str(x + 1) + "," + str(y))
             
         return neighbors
